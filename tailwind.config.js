a372793f15b23d057e7f4ca01813a71da593a801
Vincelicious <hello@vincentvandeursen.com>
2020-03-01 21:58:31 +0100

    Added first draft of assignment page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,31 @@
 module.exports = {
-    theme: {},
     variants: {},
     plugins: [],
+    theme: {
+      fontFamily: {
+        sans: [
+            'Lato',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            '"Noto Sans"',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+            '"Noto Color Emoji"',
+        ],
+        mono: [
+            'Menlo',
+            'Monaco',
+            'Consolas',
+            '"Liberation Mono"',
+            '"Courier New"',
+            'monospace',
+        ],
+      }
+    }
   }
