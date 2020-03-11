dcd51dbde47dc9f9c756b892f17c842d0349ad3c
Vincelicious <hello@vincentvandeursen.com>
2020-03-11 19:53:47 +0100

    Added page transition with Barba.js

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import barba from "@barba/core";
+import gsap from "gsap";
 
 let site = new Vue({
   el: "#assignments",
@@ -6,3 +8,24 @@ let site = new Vue({
     cover: ""
   }
 })
+
+barba.init({
+  sync: true,
+  transitions: [{
+    leave() {
+      const done = this.async();
+
+      gsap.fromTo(".transition-background",
+        { y: '100%' },
+        { y: '0%', duration: .2, ease: "circ", onComplete: () => done() }
+      )
+    },
+    after() {
+      const done = this.async();
+
+      gsap.to(".transition-background",
+        { y: '-100%', duration: .2, delay: .3, ease: "circ", onComplete: () => done() }
+      )
+    }
+  }]
+});
