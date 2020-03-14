9e6a48e529f306aa32f9d0ce25ebedf473db3086
Vincelicious <hello@vincentvandeursen.com>
2020-03-14 11:18:40 +0100

    Removed code for Algolia instant search

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,9 +1,13 @@
 import barba from "@barba/core";
 import gsap from "gsap";
+import "alpinejs";
 
+// Page transition with Barba.js and GSAP
+// ----------------------------------------
 barba.init({
-  // sync: true,
   // debug: true,
+  cacheIgnore: true,
+  prefetchIgnore: true,
   transitions: [{
     leave() {
       const done = this.async();
@@ -22,38 +26,3 @@ barba.init({
     }
   }]
 });
-
-// const searchClient = algoliasearch('GAHT5OTCHA', 'e237c3400dab85b69d126de1bcad9ea8');
-
-// const search = instantsearch({
-//   indexName: 'assignments',
-//   searchClient,
-// });
-
-// search.addWidgets([
-//   instantsearch.widgets.searchBox({
-//     container: '#searchbox',
-//     placeholder: 'Search'
-//   }),
-
-//   instantsearch.widgets.hits({
-//     container: '#hits',
-//     templates: {
-//       item: `
-//         <li class="flex items-baseline border-b px-8 pb-2 mb-10">
-//           <div class="w-64">
-//             <span class="opacity-25 dot-after">{{ date }}</span>
-//             <a  href="#" class="opacity-25">{{ class }}</a>
-//           </div>
-//           <a href="{{ url }}">
-//             <h4 class="assignment-title text-3xl relative" @mouseover='cover = "/assets/{{ cover }}"'>
-//               {{ title }}
-//             </h4>
-//           </a>
-//         </li>
-//       `,
-//     },
-//   })
-// ]);
-
-// search.start();
